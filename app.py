affaf7bc1585861458a080ee35842a7ae9a071b2
zhuang <GrayShine@users.noreply.huggingface.co>
2024-01-21 09:58:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,9 @@ import torchvision
 from PIL import Image
 from transformers import CLIPVisionModelWithProjection, CLIPImageProcessor
 from transformers.image_transforms import convert_to_rgb
+import spaces
     
-+@spaces.GPU
+@spaces.GPU
 def auto_inpainting(video_input, masked_video, mask, prompt, image, vae, text_encoder, image_encoder, diffusion, model, device, cfg_scale, img_cfg_scale, negative_prompt=""):
     global use_fp16
     image_prompt_embeds = None
@@ -82,7 +83,7 @@ def auto_inpainting(video_input, masked_video, mask, prompt, image, vae, text_en
     video_clip = vae.decode(video_clip / 0.18215).sample # [16, 3, 256, 256]
     return video_clip
 
-+@spaces.GPU
+@spaces.GPU
 def auto_inpainting_temp_split(video_input, masked_video, mask, prompt, image, vae, text_encoder, image_encoder, diffusion, model, device, scfg_scale, tcfg_scale, img_cfg_scale, negative_prompt=""):
     global use_fp16
     image_prompt_embeds = None
@@ -153,7 +154,7 @@ vae = None
 text_encoder = None
 image_encoder = None
 clip_image_processor = None
-+@spaces.GPU
+@spaces.GPU
 def init_model():
     global device
     global output_path
@@ -212,7 +213,7 @@ init_model()
 # ========================================
 #             Video Generation
 # ========================================
-+@spaces.GPU
+@spaces.GPU
 def video_generation(text, image, scfg_scale, tcfg_scale, img_cfg_scale, diffusion):
     with torch.no_grad():
         print("begin generation", flush=True)
@@ -242,6 +243,7 @@ def video_generation(text, image, scfg_scale, tcfg_scale, img_cfg_scale, diffusi
 # ========================================
 #             Video Prediction
 # ========================================
+@spaces.GPU
 def video_prediction(text, image, scfg_scale, tcfg_scale, img_cfg_scale, preframe, diffusion):
     with torch.no_grad():
         print("begin generation", flush=True)
@@ -278,7 +280,7 @@ def video_prediction(text, image, scfg_scale, tcfg_scale, img_cfg_scale, prefram
 # ========================================
 #      Judge Generation or Prediction
 # ========================================
-+@spaces.GPU
+@spaces.GPU
 def gen_or_pre(text_input, image_input, scfg_scale, tcfg_scale, img_cfg_scale, preframe_input, diffusion_step):
     default_step = [25, 40, 50, 100, 125, 200, 250]
     difference = [abs(item - diffusion_step) for item in default_step]
